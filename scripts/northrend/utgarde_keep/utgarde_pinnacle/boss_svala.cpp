87f3e39a2382756142b746adee535c8948053629
Thunder SAM' <el.khalsami@hotmail.fr>
2010-11-12 16:02:27 -0800

    COMPILABLE =D

--- scripts/northrend/utgarde_keep/utgarde_pinnacle/boss_svala.cpp
+++ scripts/northrend/utgarde_keep/utgarde_pinnacle/boss_svala.cpp
@@ -1,3 +1,4 @@
+
 /* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,8 +17,8 @@
 
 /* ScriptData
 SDName: Boss_Svala
-SD%Complete: 30%
-SDComment: TODO: abilities. The way spells for intro works could use more research.
+SD%Complete: 90%
+SDComment: 
 SDCategory: Utgarde Pinnacle
 EndScriptData */
 
@@ -45,10 +46,13 @@ enum
 
     NPC_SVALA_SORROW            = 26668,
     NPC_ARTHAS_IMAGE            = 29280,
+    NPC_RITUAL_CHANNELER        = 27281,
+    NPC_RITUAL_TARGET           = 27327,
+    NPC_FLAME_BRAZIER           = 27273,
 
     SPELL_ARTHAS_VISUAL         = 54134,
 
-    // don't know how these should work in relation to each other
+    // Sorts .
     SPELL_TRANSFORMING          = 54205,
     SPELL_TRANSFORMING_FLOATING = 54140,
     SPELL_TRANSFORMING_CHANNEL  = 54142,
@@ -56,7 +60,86 @@ enum
     SPELL_RITUAL_OF_SWORD       = 48276,
     SPELL_CALL_FLAMES           = 48258,
     SPELL_SINISTER_STRIKE       = 15667,
-    SPELL_SINISTER_STRIKE_H     = 59409
+    SPELL_SINISTER_STRIKE_H     = 59409,
+    SPELL_BALL_OF_FLAME         = 48246,
+
+    //TECHNIQUES A PART .
+    SPELL_RITUAL_OF_SWORD_RS    = 54159, 
+    SPELL_RITUAL_OF_SWORD_UNK   = 54148, 
+    SPELL_RITUAL_STRIKE         = 48277, 
+    SPELL_SWIRL_SWORD           = 48331, 
+
+    // Buff , et stun .
+    SPELL_PARALYZE              = 48278,
+    SPELL_SHADOWS_IN_THE_DARK   = 59407,
+
+    MODEL_ID_INVISIBLE          = 11686,
+
+    NORMAL                      = 1,
+    SACRED                      = 2
+};
+
+struct Locations
+{
+    float x, y, z;
+    uint32 id;
+};
+
+static Locations RitualChannelerPos[]=
+{
+    {296.42f, -355.01f, 90.94f},
+    {302.36f, -352.01f, 90.54f},
+    {291.39f, -350.89f, 90.54f}
+};
+
+/*######
+## npc_ritual_channeler
+######*/
+
+struct MANGOS_DLL_DECL npc_ritual_channelerAI : public Scripted_NoMovementAI
+{
+    npc_ritual_channelerAI(Creature* pCreature) : Scripted_NoMovementAI(pCreature)
+    {
+        m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
+        Reset();
+    }
+
+    bool m_bIsRegularMode;
+
+    Unit* m_pTarget;
+    bool m_bCasted;
+
+    void Reset()
+    {
+        m_bCasted = false;
+        if (!m_bIsRegularMode)
+            DoCast(m_creature, SPELL_SHADOWS_IN_THE_DARK);
+    }
+
+    void SetVictim(Unit* pVictim)
+    {
+         if (pVictim)
+            m_pTarget = pVictim;
+    }
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        if (m_pTarget && (!m_bCasted || !m_pTarget->HasAura(SPELL_PARALYZE)))
+        {
+            SpellEntry* pTempSpell = (SpellEntry*)GetSpellStore()->LookupEntry(SPELL_PARALYZE);
+            if (pTempSpell)
+            {
+                pTempSpell->EffectImplicitTargetA[0] = TARGET_EFFECT_SELECT;
+                pTempSpell->EffectImplicitTargetB[0] = 0;
+                pTempSpell->EffectImplicitTargetA[1] = TARGET_EFFECT_SELECT;
+                pTempSpell->EffectImplicitTargetB[1] = 0;
+                pTempSpell->EffectImplicitTargetA[2] = TARGET_EFFECT_SELECT;
+                pTempSpell->EffectImplicitTargetB[2] = 0;
+                m_creature->CastSpell(m_pTarget, pTempSpell, true);
+            }
+            m_bCasted = true;
+        }
+    }
 };
 
 /*######
@@ -81,6 +164,15 @@ struct MANGOS_DLL_DECL boss_svalaAI : public ScriptedAI
     bool m_bIsIntroDone;
     uint32 m_uiIntroTimer;
     uint32 m_uiIntroCount;
+    uint32 m_uiSinsterStrikeTimer;
+    uint32 m_uiCallFlamesTimer;
+    uint32 m_uiRitualOfSwordTimer;
+    uint32 m_uiSacrificeTimer;
+    uint32 m_uiRitualStage;
+
+    uint8 m_uiPhase;
+
+    Unit* pVictim;
 
     void Reset()
     {
@@ -88,6 +180,22 @@ struct MANGOS_DLL_DECL boss_svalaAI : public ScriptedAI
 
         m_uiIntroTimer = 2500;
         m_uiIntroCount = 0;
+        m_uiRitualStage = 1;
+        m_uiSinsterStrikeTimer = 4000;
+        m_uiCallFlamesTimer = 10000;
+        m_uiRitualOfSwordTimer = 20000;
+        m_uiSacrificeTimer = 25000;
+
+        m_uiPhase = NORMAL;
+
+        m_creature->NearTeleportTo(296.632f, -346.075f, 90.630f, 0);
+        m_creature->RemoveSplineFlag(SPLINEFLAG_FLYING);
+        m_creature->SetByteValue(UNIT_FIELD_BYTES_1, 3, 0);
+        m_creature->clearUnitState(UNIT_STAT_STUNNED | UNIT_STAT_ROOT);
+
+        pVictim = NULL;
+        m_creature->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 0, uint32(37146));
+        m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
 
         if (m_creature->isAlive() && m_pInstance && m_pInstance->GetData(TYPE_SVALA) > IN_PROGRESS)
         {
@@ -95,29 +203,24 @@ struct MANGOS_DLL_DECL boss_svalaAI : public ScriptedAI
                 m_creature->UpdateEntry(NPC_SVALA_SORROW);
 
             m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
-
             m_bIsIntroDone = true;
         }
     }
 
-    void JustReachedHome()
-    {
-        DoMoveToPosition();
-    }
-
     void MoveInLineOfSight(Unit* pWho)
     {
-        if (!m_bIsIntroDone)
+        if (!m_bIsIntroDone && pWho && pWho->GetTypeId() == TYPEID_PLAYER && !((Player*)pWho)->isGameMaster())
         {
             if (m_pInstance && m_pInstance->GetData(TYPE_SVALA) == IN_PROGRESS)
             {
                 m_pInstance->SetData(TYPE_SVALA, SPECIAL);
+                m_creature->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 0, uint32(37146));
 
                 float fX, fY, fZ;
                 m_creature->GetClosePoint(fX, fY, fZ, m_creature->GetObjectBoundingRadius(), 16.0f, 0.0f);
 
-                // we assume m_creature is spawned in proper location
-                m_creature->SummonCreature(NPC_ARTHAS_IMAGE, fX, fY, fZ, 0.0f, TEMPSUMMON_TIMED_DESPAWN, 60000);
+                
+                m_creature->SummonCreature(NPC_ARTHAS_IMAGE, fX, fY, fZ, 0.0f, TEMPSUMMON_TIMED_DESPAWN, 50000);
             }
 
             return;
@@ -126,11 +229,17 @@ struct MANGOS_DLL_DECL boss_svalaAI : public ScriptedAI
         ScriptedAI::MoveInLineOfSight(pWho);
     }
 
+    void AttackStart(Unit* pWho)
+    {
+        if (m_uiPhase != NORMAL) return;
+ 
+        ScriptedAI::AttackStart(pWho);
+    }
+
     void Aggro(Unit* pWho)
     {
-        if (m_creature->HasSplineFlag(SPLINEFLAG_FLYING))
-            m_creature->RemoveSplineFlag(SPLINEFLAG_FLYING);
-
+        m_creature->RemoveSplineFlag(SPLINEFLAG_FLYING);
+        m_creature->SetByteValue(UNIT_FIELD_BYTES_1, 3, 0);
         DoScriptText(SAY_AGGRO, m_creature);
     }
 
@@ -150,6 +259,25 @@ struct MANGOS_DLL_DECL boss_svalaAI : public ScriptedAI
             pArthas = NULL;
     }
 
+    void DoMoveToPosition()
+    {
+        float fX, fZ, fY;
+        m_creature->GetRespawnCoord(fX, fY, fZ);
+
+        m_creature->AddSplineFlag(SPLINEFLAG_FLYING);
+        m_creature->SetByteValue(UNIT_FIELD_BYTES_1, 3, UNIT_BYTE1_FLAG_ALWAYS_STAND | UNIT_BYTE1_FLAG_UNK_2);
+
+        m_creature->SendMonsterMoveWithSpeed(fX, fY, fZ + 5.0f, m_uiIntroTimer);
+        m_creature->GetMap()->CreatureRelocation(m_creature, fX, fY, fZ + 5.0f, m_creature->GetOrientation());
+    }
+
+    void SpellHitTarget(Unit *target, const SpellEntry *spell)
+    {
+        if (spell->Id == SPELL_SWIRL_SWORD)
+            if(target->GetTypeId() != TYPEID_PLAYER && target->GetEntry() == NPC_RITUAL_TARGET)
+                m_creature->CastSpell(target, SPELL_RITUAL_STRIKE, true);
+    }
+
     void SpellHit(Unit* pCaster, const SpellEntry* pSpell)
     {
         if (pSpell->Id == SPELL_TRANSFORMING)
@@ -158,6 +286,9 @@ struct MANGOS_DLL_DECL boss_svalaAI : public ScriptedAI
                 pArthas->InterruptNonMeleeSpells(true);
 
             m_creature->UpdateEntry(NPC_SVALA_SORROW);
+            m_creature->RemoveAurasDueToSpell(SPELL_TRANSFORMING_FLOATING);
+            m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+            m_creature->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 0, uint32(37146));
         }
     }
 
@@ -179,73 +310,201 @@ struct MANGOS_DLL_DECL boss_svalaAI : public ScriptedAI
             m_pInstance->SetData(TYPE_SVALA, DONE);
     }
 
-    void DoMoveToPosition()
+    void SacredText(uint8 SoundId)
     {
-        float fX, fZ, fY;
-        m_creature->GetRespawnCoord(fX, fY, fZ);
-
-        m_creature->AddSplineFlag(SPLINEFLAG_FLYING);
-
-        m_creature->SendMonsterMoveWithSpeed(fX, fY, fZ + 5.0f, m_uiIntroTimer);
-        m_creature->GetMap()->CreatureRelocation(m_creature, fX, fY, fZ + 5.0f, m_creature->GetOrientation());
+        switch(SoundId)
+        {
+           case 1:
+              DoScriptText(SAY_SACRIFICE_1, m_creature);
+              break;
+           case 2:
+              DoScriptText(SAY_SACRIFICE_2, m_creature);
+              break;
+           case 3:
+              DoScriptText(SAY_SACRIFICE_3, m_creature);
+              break;
+           case 4:
+              DoScriptText(SAY_SACRIFICE_4, m_creature);
+              break;
+           case 5:
+              DoScriptText(SAY_SACRIFICE_5, m_creature);
+              break;
+        }
     }
 
     void UpdateAI(const uint32 uiDiff)
     {
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
         {
-            if (m_bIsIntroDone)
+            if (m_bIsIntroDone || (!pArthas && m_uiIntroCount < 5))
                 return;
 
-            if (pArthas && pArthas->isAlive())
+            if (m_uiIntroTimer < uiDiff)
             {
-                if (m_uiIntroTimer < uiDiff)
+                switch (m_uiIntroCount)
                 {
-                    m_uiIntroTimer = 10000;
-
-                    switch(m_uiIntroCount)
-                    {
-                        case 0:
-                            DoScriptText(SAY_INTRO_1, m_creature);
-                            break;
-                        case 1:
-                            DoScriptText(SAY_INTRO_2_ARTHAS, pArthas);
-                            break;
-                        case 2:
-                            pArthas->CastSpell(m_creature, SPELL_TRANSFORMING_CHANNEL, false);
-                            m_creature->CastSpell(m_creature, SPELL_TRANSFORMING_FLOATING, false);
-                            DoMoveToPosition();
-                            break;
-                        case 3:
-                            m_creature->CastSpell(m_creature, SPELL_TRANSFORMING, false);
-                            DoScriptText(SAY_INTRO_3, m_creature);
-                            break;
-                        case 4:
-                            DoScriptText(SAY_INTRO_4_ARTHAS, pArthas);
-                            break;
-                        case 5:
-                            DoScriptText(SAY_INTRO_5, m_creature);
-                            m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
-                            m_bIsIntroDone = true;
-                            break;
-                    }
-
-                    ++m_uiIntroCount;
+                    case 0:
+                        m_creature->SetStandState(UNIT_STAND_STATE_KNEEL);
+                        DoScriptText(SAY_INTRO_1, m_creature);
+                        m_uiIntroTimer = 9000;
+                        break;
+                    case 1:
+                        DoScriptText(SAY_INTRO_2_ARTHAS, pArthas);
+                        m_uiIntroTimer = 12000;
+                        break;
+                    case 2:
+                        m_creature->SetStandState(UNIT_STAND_STATE_STAND);
+                        pArthas->CastSpell(m_creature, SPELL_TRANSFORMING_CHANNEL, false);
+                        m_creature->CastSpell(m_creature, SPELL_TRANSFORMING_FLOATING, false);
+                        m_creature->SetByteValue(UNIT_FIELD_BYTES_1, 3, UNIT_BYTE1_FLAG_ALWAYS_STAND | UNIT_BYTE1_FLAG_UNK_2);
+                        m_uiIntroTimer = 6000;
+                        DoMoveToPosition();
+                        break;
+                    case 3:
+                        m_creature->CastSpell(m_creature, SPELL_TRANSFORMING, false);
+                        DoScriptText(SAY_INTRO_3, m_creature);
+                        m_uiIntroTimer = 12000;
+                        break;
+                    case 4:
+                        DoScriptText(SAY_INTRO_4_ARTHAS, pArthas);
+                        m_uiIntroTimer = 7000;
+                        break;
+                    case 5:
+                        DoScriptText(SAY_INTRO_5, m_creature);
+                        m_uiIntroTimer = 13000;
+                        break;
+                    case 6:
+                        m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+                        m_bIsIntroDone = true;
+                        break;
                 }
-                else
-                    m_uiIntroTimer -= uiDiff;
+                ++m_uiIntroCount;
             }
+            else
+                m_uiIntroTimer -= uiDiff;
 
             return;
         }
+        else
+        {
+            if (m_uiPhase == NORMAL)
+            {
+                if (m_uiSinsterStrikeTimer <= uiDiff)
+                {
+                    DoCast(m_creature->getVictim(), m_bIsRegularMode ? SPELL_SINISTER_STRIKE : SPELL_SINISTER_STRIKE_H);
+                    m_uiSinsterStrikeTimer = urand(5000, 9000);
+                }
+                else
+                    m_uiSinsterStrikeTimer -= uiDiff;
 
-        DoMeleeAttackIfReady();
+                if (m_uiCallFlamesTimer <= uiDiff)
+                {
+                    DoCast(m_creature, SPELL_CALL_FLAMES);
+                    int32 dmg = urand(1000, 1500);
+                    if (!m_bIsRegularMode)
+                        dmg += 1500;
+                    Creature* pBrazier = NULL;
+                    if (pBrazier = m_creature->SummonCreature(NPC_FLAME_BRAZIER, 285.64f, -355.16f, 92.99f, 1.65f, TEMPSUMMON_TIMED_DESPAWN, 5000))
+                    {
+                        pBrazier->SetDisplayId(MODEL_ID_INVISIBLE);
+                        pBrazier->setFaction(14);
+                        pBrazier->CastCustomSpell(pBrazier, SPELL_BALL_OF_FLAME, &dmg, 0, 0, true);
+                    }
+                    if (pBrazier = m_creature->SummonCreature(NPC_FLAME_BRAZIER, 306.94f, -355.77f, 92.96f, 1.65f, TEMPSUMMON_TIMED_DESPAWN, 5000))
+                    {
+                        pBrazier->SetDisplayId(MODEL_ID_INVISIBLE);
+                        pBrazier->setFaction(14);
+                        pBrazier->CastCustomSpell(pBrazier, SPELL_BALL_OF_FLAME, &dmg, 0, 0, true);
+                    }
+                    m_uiCallFlamesTimer = urand(8000,12000);
+                }
+                else
+                    m_uiCallFlamesTimer -= uiDiff;
+
+                if (m_uiRitualOfSwordTimer <= uiDiff)
+                { 
+                    if (Unit* pSacrificeTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                    {
+                        m_creature->InterruptNonMeleeSpells(false);
+                        SacredText(urand(1, 5));
+                        m_creature->CastSpell(pSacrificeTarget, SPELL_RITUAL_OF_SWORD, true); //spell ports caster but not target
+                        pVictim = pSacrificeTarget;
+                        m_uiSacrificeTimer = 750;
+                        m_uiPhase = SACRED;
+                        m_uiRitualStage = 1;
+                    }
+                }
+                else
+                    m_uiRitualOfSwordTimer -= uiDiff;
+
+                DoMeleeAttackIfReady();
+            }
+            else  
+            {
+                if (m_uiSacrificeTimer <= uiDiff)
+                {
+                    switch(m_uiRitualStage)
+                    {
+                        case 1:
+                            m_creature->AddSplineFlag(SPLINEFLAG_FLYING);
+                            m_creature->NearTeleportTo(296.632f, -346.075f, 90.63f + 17.0f, 0);
+                            if (pVictim)
+                            {
+                                DoTeleportPlayer(pVictim, 296.632f, -346.075f, 90.63f, 4.6f);
+                                for (uint8 i = 0; i < 3; ++i)
+                                    if (Creature* pSummon = m_creature->SummonCreature(NPC_RITUAL_CHANNELER, RitualChannelerPos[i].x,RitualChannelerPos[i].y,RitualChannelerPos[i].z, 0, TEMPSUMMON_TIMED_OR_CORPSE_DESPAWN, 27000))
+                                        ((npc_ritual_channelerAI*)pSummon->AI())->SetVictim(pVictim);
+                                m_creature->SetFacingToObject(pVictim);
+                            }
+                            m_creature->addUnitState(UNIT_STAT_STUNNED | UNIT_STAT_ROOT);
+                            m_uiSacrificeTimer = 100;
+                            ++m_uiRitualStage;
+                            break;
+                        case 2:
+                            m_creature->NearTeleportTo(296.632f, -346.075f, 90.63f + 17.0f, 0);
+                            m_uiSacrificeTimer = 100;
+                            ++m_uiRitualStage;
+                            break;
+                        case 3:
+                            if (Creature* pRitualTarget = m_creature->SummonCreature(NPC_RITUAL_TARGET, 296.632f, -346.075f, 90.63f, 1.568f, TEMPSUMMON_TIMED_DESPAWN, 30000))
+                            {
+                                pRitualTarget->SetDisplayId(MODEL_ID_INVISIBLE);
+                                m_creature->CastSpell(pRitualTarget, SPELL_SWIRL_SWORD, true);
+                            }
+                            m_uiSacrificeTimer = 100;
+                            ++m_uiRitualStage;
+                            break;
+                        case 4:
+                            m_creature->CastSpell(m_creature, SPELL_RITUAL_OF_SWORD_RS, true);
+                            m_uiSacrificeTimer = 26000;
+                            ++m_uiRitualStage;
+                            break;
+                        case 5:
+                            m_uiPhase = NORMAL;
+                            pVictim = NULL;
+                            m_creature->clearUnitState(UNIT_STAT_STUNNED | UNIT_STAT_ROOT);
+                            m_creature->InterruptNonMeleeSpells(false);
+                            m_creature->RemoveSplineFlag(SPLINEFLAG_FLYING);
+                            m_uiRitualOfSwordTimer = 20000;
+                            m_uiRitualStage = 0;
+                            break;
+                    }
+                }
+                else
+                    m_uiSacrificeTimer -= uiDiff;
+            }
+        }
     }
 };
 
 CreatureAI* GetAI_boss_svala(Creature* pCreature)
 {
     return new boss_svalaAI(pCreature);
+}
+
+CreatureAI* GetAI_npc_ritual_channeler(Creature* pCreature)
+{
+    return new npc_ritual_channelerAI(pCreature);
 }
 
 bool AreaTrigger_at_svala_intro(Player* pPlayer, AreaTriggerEntry const* pAt)
@@ -266,6 +525,11 @@ void AddSC_boss_svala()
     newscript = new Script;
     newscript->Name = "boss_svala";
     newscript->GetAI = &GetAI_boss_svala;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_ritual_channeler";
+    newscript->GetAI = &GetAI_npc_ritual_channeler;
     newscript->RegisterSelf();
 
     newscript = new Script;
